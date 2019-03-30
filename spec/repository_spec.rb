6bcbee498efeb4080d3230b388c6e02214a8275e
cuurjol <cuurjol@gmail.com>
2019-03-31 00:25:02 +0300

    Update Rspec tests.

--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -6,17 +6,50 @@ require_relative '../lib/wardrobe'
 require_relative '../lib/repository'
 
 describe Repository do
-  let(:xml_file_path) { File.absolute_path(File.join(data_folder_path, 'clothing.xml')) }
-  let(:data_folder_path) { File.join(Dir.pwd, 'spec', 'fixtures') }
-  let(:repository) { Repository.new(xml_file_path) }
+  before(:all) do
+    document_xml = REXML::Document.new('<?xml version="1.0" encoding="utf-8"?>')
+    document_xml.add_element('clothing')
+    garment_count = rand(1..10)
 
-  before(:example) do
+    1.upto(garment_count) do |id|
+      name = 'Random name ' + rand(1..100).to_s
+      type = 'Random type ' + rand(1..100).to_s
+      temperature = "(#{rand(-100..0)}, +#{rand(1..100)})"
+
+      garment_node = document_xml.root.add_element('garment')
+      garment_node.add_attribute('id', id.to_s)
+
+      name_node = garment_node.elements.add('name')
+      name_node.text = name
+
+      type_node = garment_node.elements.add('type')
+      type_node.text = type
+
+      temperature_node = garment_node.elements.add('temperature')
+      temperature_node.text = temperature
+    end
+
+    data_folder_path = File.join(Dir.pwd, 'spec', 'fixtures')
     FileUtils.mkdir_p(data_folder_path)
-    FileUtils.cp(File.absolute_path('./data/clothing.xml'), data_folder_path)
-    @quantity = repository.instance_variable_get(:@data_hash).size
+    FileUtils.touch(File.join(data_folder_path, 'clothing.xml'))
+    xml_file_path = File.absolute_path(File.join(data_folder_path, 'clothing.xml'))
+
+    file = File.new(File.join(data_folder_path, 'clothing.xml'), 'w:UTF-8')
+    formatter = REXML::Formatters::Pretty.new
+    formatter.compact = true
+    formatter.write(document_xml, file)
+    file.close
+
+    @repository = Repository.new(xml_file_path)
   end
 
-  after(:example) do
+  before(:each) do
+    @quantity = @repository.instance_variable_get(:@data_hash).size
+  end
+
+  after(:all) do
+    data_folder_path = File.join(Dir.pwd, 'spec', 'fixtures')
+    xml_file_path = File.absolute_path(File.join(data_folder_path, 'clothing.xml'))
     FileUtils.rm(xml_file_path)
     FileUtils.remove_dir(data_folder_path)
   end
@@ -24,23 +57,23 @@ describe Repository do
   describe '#add' do
     it 'adds a new garment node' do
       garment = Garment.new('Свитер', 'Верхняя одежда', Range.new(-15, 0))
-      repository.add(garment)
-      expect(repository.instance_variable_get(:@data_hash).size).to be > @quantity
+      @repository.add(garment)
+      expect(@repository.instance_variable_get(:@data_hash).size).to be > @quantity
     end
   end
 
   describe '#delete' do
     it 'deletes an existing garment node' do
-      wardrobe = Wardrobe.new(repository.get_data)
-      garment = wardrobe.search(by: :name, value: 'Панама')
-      repository.delete(garment)
-      expect(repository.instance_variable_get(:@data_hash).size).to be < @quantity
+      wardrobe = Wardrobe.new(@repository.get_data)
+      garment = wardrobe.clothing.sample
+      @repository.delete(garment)
+      expect(@repository.instance_variable_get(:@data_hash).size).to be < @quantity
     end
 
     it 'tries to delete a non existing garment node' do
-      garment = Garment.new('Красная куртка', 'Верхняя одежда', Range.new(0, 10))
-      repository.delete(garment)
-      expect(repository.instance_variable_get(:@data_hash).size).to eq @quantity
+      garment = Garment.new('Random garment qwerty', 'Random type qwerty', Range.new(-150_000, 250_000))
+      @repository.delete(garment)
+      expect(@repository.instance_variable_get(:@data_hash).size).to eq @quantity
     end
   end
 
@@ -48,14 +81,14 @@ describe Repository do
     describe '#left_limit_range' do
       it 'returns the starting value of temperature range' do
         line = '(-20, 0)'
-        expect(repository.send(:left_limit_range, line)).to eq -20
+        expect(@repository.send(:left_limit_range, line)).to eq -20
       end
     end
 
     describe '#right_limit_range' do
       it 'returns the ending value of temperature range' do
         line = '(-20, 0)'
-        expect(repository.send(:right_limit_range, line)).to eq 0
+        expect(@repository.send(:right_limit_range, line)).to eq 0
       end
     end
 
@@ -63,7 +96,7 @@ describe Repository do
       it 'returns the created information' do
         garment = Garment.new('Свитер', 'Верхняя одежда', Range.new(-15, 0))
         test_information = [garment.name, garment.type, '(-15, 0)']
-        expect(repository.send(:create_information_array, garment)).to eq test_information
+        expect(@repository.send(:create_information_array, garment)).to eq test_information
       end
     end
   end
--- spec/wardrobe_spec.rb
+++ spec/wardrobe_spec.rb
@@ -5,24 +5,64 @@ require_relative '../lib/garment'
 require_relative '../lib/wardrobe'
 
 describe Wardrobe do
-  let(:xml_file_path) { File.absolute_path(File.join(data_folder_path, 'clothing.xml')) }
-  let(:data_folder_path) { File.join(Dir.pwd, 'spec', 'fixtures') }
-  let(:wardrobe) { Wardrobe.new(@repository.get_data) }
+  before(:all) do
+    document_xml = REXML::Document.new('<?xml version="1.0" encoding="utf-8"?>')
+    document_xml.add_element('clothing')
+    garment_array = [
+      Garment.new('Шапка-ушанка', 'Головной убор', Range.new(-20, -5)),
+      Garment.new('Кроссовки', 'Обувь', Range.new(0, 15)),
+      Garment.new('Джинсы', 'Штаны', Range.new(-5, 15)),
+      Garment.new('Пальто', 'Верхняя одежда', Range.new(-5, 10)),
+      Garment.new('Шлепанцы', 'Обувь', Range.new(20, 40)),
+      Garment.new('Шорты-карго', 'Шорты', Range.new(20, 30)),
+      Garment.new('Плавки', 'Шорты', Range.new(30, 40)),
+      Garment.new('Панама', 'Головной убор', Range.new(30, 40)),
+      Garment.new('Белая футболка', 'Футболка', Range.new(20, 40))
+    ]
 
-  before(:example) do
+    garment_array.each_with_index do |garment, index|
+      name = garment.name
+      type = garment.type
+      temperature = "(#{garment.temperature_range.begin}, +#{garment.temperature_range.end})"
+
+      garment_node = document_xml.root.add_element('garment')
+      garment_node.add_attribute('id', (index + 1).to_s)
+
+      name_node = garment_node.elements.add('name')
+      name_node.text = name
+
+      type_node = garment_node.elements.add('type')
+      type_node.text = type
+
+      temperature_node = garment_node.elements.add('temperature')
+      temperature_node.text = temperature
+    end
+
+    data_folder_path = File.join(Dir.pwd, 'spec', 'fixtures')
     FileUtils.mkdir_p(data_folder_path)
-    FileUtils.cp(File.absolute_path('./data/clothing.xml'), data_folder_path)
+    FileUtils.touch(File.join(data_folder_path, 'clothing.xml'))
+    xml_file_path = File.absolute_path(File.join(data_folder_path, 'clothing.xml'))
+
+    file = File.new(File.join(data_folder_path, 'clothing.xml'), 'w:UTF-8')
+    formatter = REXML::Formatters::Pretty.new
+    formatter.compact = true
+    formatter.write(document_xml, file)
+    file.close
+
     @repository = Repository.new(xml_file_path)
+    @wardrobe = Wardrobe.new(@repository.get_data)
   end
 
-  after(:example) do
+  after(:all) do
+    data_folder_path = File.join(Dir.pwd, 'spec', 'fixtures')
+    xml_file_path = File.absolute_path(File.join(data_folder_path, 'clothing.xml'))
     FileUtils.rm(xml_file_path)
     FileUtils.remove_dir(data_folder_path)
   end
 
   describe '#new' do
     it 'assigns instance variables' do
-      expect(wardrobe.clothing).not_to be_empty
+      expect(@wardrobe.clothing).not_to be_empty
     end
 
     it 'assigns nil instance variables' do
@@ -32,68 +72,67 @@ describe Wardrobe do
   end
 
   describe 'Add/delete methods' do
-    let(:garment) { Garment.new("Халат", "Верхняя одежда", Range.new(15, 25)) }
-    let!(:quantity) { wardrobe.clothing.size }
+    let!(:quantity) { @wardrobe.clothing.size }
 
     describe '#add' do
       it 'adds a new garment to the wardrobe' do
-        wardrobe.add(garment)
-        expect(wardrobe.clothing.size).to be > quantity
+        @wardrobe.add(Garment.new('Кепка Adidas', 'Головной убор', Range.new(10, 30)))
+        expect(@wardrobe.clothing.size).to be > quantity
       end
     end
 
     describe '#delete' do
       it 'deletes the garment from the wardrobe' do
-        garment = wardrobe.search(by: :name, value: "Пальто")
-        wardrobe.delete(garment)
-        expect(wardrobe.clothing.size).to be < quantity
+        garment = @wardrobe.clothing.first
+        @wardrobe.delete(garment)
+        expect(@wardrobe.clothing.size).to be < quantity
       end
 
       it 'deletes the non existing garment from the wardrobe' do
-        wardrobe.delete(garment)
-        expect(wardrobe.clothing.size).to eq quantity
+        @wardrobe.delete(Garment.new('Random garment', 'Random type', Range.new(-5, 5)))
+        expect(@wardrobe.clothing.size).to eq quantity
       end
     end
   end
 
   describe '#search' do
     it 'returns first garment called "Шлепанцы"' do
-      expect(wardrobe.search(by: :name, value: "Шлепанцы")).to be_truthy
+      expect(@wardrobe.search(by: :name, value: 'Шлепанцы')).to be_truthy
     end
 
-    it 'doesn\'t returns first garment with a type called "Халат"' do
-      expect(wardrobe.search(by: :type, value: "Халат")).to be_falsey
+    it 'doesn\'t return first garment with a type called "Халат"' do
+      expect(@wardrobe.search(by: :type, value: 'Халат')).to be_falsey
     end
 
     it 'returns first garment with a temperature range of -5 to +10' do
-      expect(wardrobe.search(by: :temperature_range, value: Range.new(-5, 10))).to be_truthy
+      expect(@wardrobe.search(by: :temperature_range, value: Range.new(-5, 10))).to be_truthy
     end
   end
 
   describe '#search_all' do
     it 'returns all garments with a type called "Обувь"' do
-      expect(wardrobe.search_all(by: :type, value: "Обувь")).not_to be_empty
+      expect(@wardrobe.search_all(by: :type, value: 'Обувь')).not_to be_empty
     end
 
     it 'doesn\'t return all garments with a temperature range of -5 to 5' do
-      expect(wardrobe.search_all(by: :temperature_range, value: Range.new(-5, 5))).to be_empty
+      expect(@wardrobe.search_all(by: :temperature_range, value: Range.new(-5, 5))).to be_empty
     end
   end
 
   describe '#what_to_wear' do
     it 'returns a final clothing' do
-      expect(wardrobe.what_to_wear(20)).not_to be_empty
+      expect(@wardrobe.what_to_wear(20)).not_to be_empty
     end
 
     it 'doesn\'t return a final clothing' do
-      expect(wardrobe.what_to_wear(-30)).to be_empty
+      expect(@wardrobe.what_to_wear(-30)).to be_empty
     end
   end
 
   describe 'private methods' do
     describe '#clothing_type' do
       it 'returns clothing of the same type' do
-        expect(wardrobe.send(:clothing_type)).not_to be_empty
+        expect(@wardrobe.send(:clothing_type)).not_to be_empty
       end
     end
   end
